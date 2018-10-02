88091d4b548ab4b6865e73416aed3ceac51c1cfb
feintKotlin <30891650+feintKotlin@users.noreply.github.com>
2018-10-03 00:46:42 +0800

    【首次提交】
    开始创建

---
+++ src/main/java/com/feint/onedayonejob/OneDayOneJobApplication.java
@@ -0,0 +1,12 @@
+package com.feint.onedayonejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDayOneJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneDayOneJobApplication.class, args);
+    }
+}
---
+++ src/test/java/com/feint/onedayonejob/OneDayOneJobApplicationTests.java
@@ -0,0 +1,16 @@
+package com.feint.onedayonejob;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OneDayOneJobApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
